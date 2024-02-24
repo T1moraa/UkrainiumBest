6785f8c2b0d639e929311582fe8f604f9d9afd74
T1moraa <133787808+T1moraa@users.noreply.github.com>
2024-02-23 22:29:14 -0800

    Update eaglercraft_opts.js

--- web/eaglercraft_opts.js
+++ web/eaglercraft_opts.js
@@ -3,8 +3,8 @@ window.eaglercraftXOpts = () => ({
   // joinServer: "ws" + window.location.href.slice(4),
   // assetsURI: "custom.epk",
 	servers: [
-		{ addr: "ws" + window.location.href.slice(4), name: "letsgoo" },
-		{ addr: "ws", name: "letsgoo" }
+		{ addr: "ws" + window.location.href.slice(4), name: "Its not work" },
+		{ addr: "wss://fictional-robot-pxqrjq9r46jfr5jp-8081.app.github.dev/", name: "Its work" }
 	]
 });
 // document.title = "My Original Server Name:tm:";
