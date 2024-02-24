eac116d21da03c220c850dfec420b1dadb73fc23
T1moraa <133787808+T1moraa@users.noreply.github.com>
2024-02-23 22:22:49 -0800

    Update eaglercraft_opts.js

--- web/eaglercraft_opts.js
+++ web/eaglercraft_opts.js
@@ -3,7 +3,8 @@ window.eaglercraftXOpts = () => ({
   // joinServer: "ws" + window.location.href.slice(4),
   // assetsURI: "custom.epk",
 	servers: [
-		{ addr: "ws" + window.location.href.slice(4), name: "EaglercraftX Server" }
+		{ addr: "ws" + window.location.href.slice(4), name: "letsgoo" },
+		{ addr: "ws", name: "letsgoo" }
 	]
 });
 // document.title = "My Original Server Name:tm:";
